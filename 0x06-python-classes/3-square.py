4a8a40ffe0f51804f6a93633a836a94f163eaf9a
Jimmer942 <942@holbertonschool.com>
2019-10-01 17:13:38 -0500

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    _size = 0
+
+    def __init__(self, new_size=0):
+        if type(new_size) is not int:
+            raise TypeError('size must be an integer')
+        if new_size < 0:
+            raise ValueError('size must be >= 0')
+        self._Square__size = new_size
+
+    def area(self):
+        return self._Square__size ** 2
