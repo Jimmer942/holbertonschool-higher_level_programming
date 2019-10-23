e3870e1feeb3ac949d06f9f36abaecd93dbf3efe
Jimmer942 <942@holbertonschool.com>
2019-10-22 22:15:58 -0400

    Update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,6 +101,16 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """ Update """
+        if len(args) >= 1:
+            super().__init__(args[0])
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) == 5:
+            self.y = args[4]
         if "id" in kwargs:
             self.id = kwargs["id"]
         if "width" in kwargs:
