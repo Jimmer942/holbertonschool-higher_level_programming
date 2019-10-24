c5b9ec72b9244d4d07c4c9d67ee866474cd48a27
Jimmer942 <942@holbertonschool.com>
2019-10-24 15:59:14 -0400

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,9 +35,9 @@ class Square(Rectangle):
             self.id = args[0]
         if len(args) >= 2:
             self.size = args[1]
-        if len(args) >= 4:
+        if len(args) >= 3:
             self.x = args[2]
-        if len(args) == 5:
+        if len(args) == 4:
             self.y = args[3]
         if "id" in kwargs:
             self.id = kwargs["id"]
