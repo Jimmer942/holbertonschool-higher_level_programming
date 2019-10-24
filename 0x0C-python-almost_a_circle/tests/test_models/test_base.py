ad505887cd1be5eefdc2bec1072a049dec76f633
Jimmer942 <942@holbertonschool.com>
2019-10-24 11:46:14 -0400

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -34,3 +34,29 @@ class TestBaseMethods(unittest.TestCase):
         self.assertEqual(new.id, 1)
         self.assertEqual(new2.id, 2)
         self.assertEqual(new3.id, 3)
+
+    def test_id_mix(self):
+        """ Test nb object attributes and assigned id """
+        new = Base()
+        new2 = Base(1024)
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 1024)
+        self.assertEqual(new3.id, 2)
+
+    def test_string_id(self):
+        """ Test string id """
+        new = Base('1')
+        self.assertEqual(new.id, '1')
+
+    def test_more_args_id(self):
+        """ Test passing more args to init method """
+        with self.assertRaises(TypeError):
+            new = Base(1, 1)
+
+    def test_access_private_attrs(self):
+        """ Test accessing to private attributes """
+        new = Base()
+        with self.assertRaises(AttributeError):
+            new.__nb_objects
+
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,30 +1,77 @@
 #!/usr/bin/python3
-""" Test Rectangle class """
+""" Test Rectangle cases """
+
 import unittest
-
+from io import StringIO
 from unittest import TestCase
+from unittest.mock import patch
 from models.rectangle import Rectangle
+from models.base import Base
 
 
-class Rectangle(unittest.TestCase):
-    """ Class Test Rectangle """
+class TestRectangleMethods(unittest.TestCase):
+    """  Test Rectangle class """
 
-    def test_id(self):
-        """ Test if the id change and it's counting"""
-        r1 = Rectangle(10, 2)
-        r2 = Rectangle(2, 10)
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r1.id, 1)
+    def setUp(self):
+        """ Base """
+        Base._Base__nb_objects = 0
+
+    def test_rectangle_1(self):
+        """ Test rectangle r1 """
+        r1 = Rectangle(10, 12)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 12)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
-        self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r2.id, 2)
+        self.assertEqual(r1.id, 1)
+
+    def test_rectangle_2(self):
+        """ Test rectangle r2 """
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 10)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r2.id, 1)
+
+    def test_rectangles_3(self):
+        """ Test new rectangles """
+        r3 = Rectangle(10, 2, 1, 1, 12)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 1)
+        self.assertEqual(r3.y, 1)
         self.assertEqual(r3.id, 12)
 
-    def test_Validate_attributes(self):
-        """ Raises the errors """
+    def test_is_Base_instance(self):
+        """ Test Rectangle is a Base instance """
+        new = Rectangle(1, 1)
+        self.assertEqual(True, isinstance(new, Base))
+
+    def test_incorrect_amount_attrs(self):
+        """ Test error raise with 1 arg passed """
         with self.assertRaises(TypeError):
-            r1 = Rectangle(10, "2")
+            new = Rectangle(1)
+
+    def test_incorrect_amount_attrs_1(self):
+        """ Test error raised with no args passed """
         with self.assertRaises(TypeError):
-            r1 = Rectangle("10", 2)
+            new = Rectangle()
+
+    def test_access_private_attrs(self):
+        """ Trying to access to a private attribute """
+        new = Rectangle(1, 1)
+        with self.assertRaises(AttributeError):
+            new.__width
+
+    def test_access_private_attrs_2(self):
+        """ Trying to access to a private attribute """
+        new = Rectangle(1, 1)
+        with self.assertRaises(AttributeError):
+            new.__height
+
+    def test_access_private_attrs_3(self):
+        """ Trying to access to a private attribute """
+        new = Rectangle(1, 1)
+        with self.assertRaises(AttributeError):
+            new.__x
