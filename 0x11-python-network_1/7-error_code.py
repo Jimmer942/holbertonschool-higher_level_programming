5b7923bc5c37e6ddb7d8f27f0ca467f59486a1c1
Jimmer942 <942@holbertonschool.com>
2020-01-13 21:15:03 -0500

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,8 +7,8 @@ import sys
 
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    req = requests.get(sys.argv[1])
+    if req.status_code == requests.codes.ok:
+        print(req.text)
     else:
-        print(r.text)
+        print("Error code: {}".format(req.status_code))
