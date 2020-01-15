eec64f08f146083bc6033a8c05565227c8ccb6e8
Jimmer942 <942@holbertonschool.com>
2020-01-15 17:21:29 -0500

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/bash
+#Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+curl -sI 0.0.0.0:5000 | grep Content-Length: |cut -d' ' -f2
