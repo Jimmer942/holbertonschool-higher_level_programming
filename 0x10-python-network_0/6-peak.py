e6f3058776589d980e74c1c3cbe99874ca32c1c0
Jimmer942 <942@holbertonschool.com>
2020-01-19 16:15:49 -0500

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
+
+""" Find peak """
+
 def find_peak(list_of_integers):
+    """ Find peak """
     if not list_of_integers:
         return None
     peak = list_of_integers[0]
