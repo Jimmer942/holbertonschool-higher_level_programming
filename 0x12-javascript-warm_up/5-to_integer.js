def3f320faec49f3fa4eacd1f26b305fc7437d4f
Jimmer942 <942@holbertonschool.com>
2020-02-04 18:36:23 -0500

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Script that prints My number: <first argument converted in integer>
+
+if (typeof process.argv[2] === 'undefined' ||
+    isNaN(parseInt(process.argv[2], 10)) === true) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2], 10));
+}
