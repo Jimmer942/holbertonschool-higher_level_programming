a40bc4fb26fbe1d805a44f3a7d683b38358bb454
Jimmer942 <942@holbertonschool.com>
2020-02-04 22:04:07 -0500

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,11 @@ if (typeof process.argv[2] === 'undefined' || process.argv.length === 3) {
   console.log(0);
 } else {
   let max = process.argv[2];
-  let max2 = 0;
+  let max2 = process.argv[2];
   for (let i = 3; i < process.argv.length; i++) {
     if (process.argv[i] > max) { max = process.argv[i]; }
   }
-  for (let i = 2; i < process.argv.length; i++) {
+  for (let i = 3; i < process.argv.length; i++) {
     if (process.argv[i] > max2 && process.argv[i] < max) {
       max2 = process.argv[i];
     }
