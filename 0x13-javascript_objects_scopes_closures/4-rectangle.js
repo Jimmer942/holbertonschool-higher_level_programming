b218bfb2e68322910df223f4688ae0d6eee77cbf
Jimmer942 <942@holbertonschool.com>
2020-02-10 18:36:57 -0500

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let st = '';
+    for (let i = 0; i < this.width; i++) { st += 'X'; }
+    for (let i = 0; i < this.height; i++) { console.log(st); }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
