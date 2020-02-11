3a7bf26a702d6a94b2d79e965c81e85b545cefc6
Jimmer942 <942@holbertonschool.com>
2020-02-10 21:18:33 -0500

    101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+function isEmpty (obj) { return Object.keys(obj).length === 0; }
+const d = {};
+for (let i = 0; ; i++) {
+  const l = [];
+  for (var key in dict) { if (dict[key] === i) { l.push(key); delete dict[key]; } }
+  if (isEmpty(l) === false) { d[i] = l; }
+  if (isEmpty(dict)) { break; }
+}
+console.log(d);
