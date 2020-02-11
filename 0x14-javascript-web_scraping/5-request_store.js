5efae7b071cf9d853109d9e975db99a44c415394
Jimmer942 <942@holbertonschool.com>
2020-02-11 14:57:34 -0500

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], function (error, response, body) {
+  if (!error) { fs.writeFileSync(process.argv[3], body, 'utf-8'); } else { console.error(error); }
+});
